6c53cb2478daeb4e28f7e0f3e5b9f3ef6700feb2
Samuel Mungy <sammi3@intim8s.cc.cc>
2017-11-19 21:23:00 +0000

    Add project files.

---
+++ SoftwareEngineering_Project/LoginPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoftwareEngineering_Project
+{
+    public partial class LoginPage : Form
+    {
+        private Login login;
+        private MainMenu loginSuccess;
+        private string staffID;
+        private string password;
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            staffID = staffIDText.Text;
+            password = passwordText.Text;
+
+            login = new Login(staffID, password);
+
+            if(login.checkLogin() == true)
+            {
+                loginSuccess = new MainMenu();
+                loginSuccess.Show();
+                this.Owner = loginSuccess;
+                this.Hide();
+            }
+        }
+    }
+}
