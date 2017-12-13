7e1455bb8642f76e3e95540646e2c5e46e765ae0
Liam Block <multilibee@gmail.com>
2017-12-13 00:16:36 +0000

    StaffTasks
    Tried to reimplement staff tasks but couldn't remember how I did it and
     DBConnection wasn't working on my PC so I could test it, we will have to see if it works in the morning..

---
+++ SoftwareEngineering_Project/StaffTasks.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace SoftwareEngineering_Project
+{
+    partial class StaffTasks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ST_Search = new System.Windows.Forms.Button();
+            this.ST_Back = new System.Windows.Forms.Button();
+            this.TB_StaffID = new System.Windows.Forms.TextBox();
+            this.ST_StaffID = new System.Windows.Forms.Label();
+            this.StaffDGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.StaffDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ST_Search
+            // 
+            this.ST_Search.Location = new System.Drawing.Point(110, 87);
+            this.ST_Search.Name = "ST_Search";
+            this.ST_Search.Size = new System.Drawing.Size(75, 23);
+            this.ST_Search.TabIndex = 0;
+            this.ST_Search.Text = "Search";
+            this.ST_Search.UseVisualStyleBackColor = true;
+            this.ST_Search.Click += new System.EventHandler(this.ST_Search_Click);
+            // 
+            // ST_Back
+            // 
+            this.ST_Back.Location = new System.Drawing.Point(12, 87);
+            this.ST_Back.Name = "ST_Back";
+            this.ST_Back.Size = new System.Drawing.Size(75, 23);
+            this.ST_Back.TabIndex = 1;
+            this.ST_Back.Text = "Back";
+            this.ST_Back.UseVisualStyleBackColor = true;
+            this.ST_Back.Click += new System.EventHandler(this.ST_Back_Click);
+            // 
+            // TB_StaffID
+            // 
+            this.TB_StaffID.Location = new System.Drawing.Point(85, 43);
+            this.TB_StaffID.Name = "TB_StaffID";
+            this.TB_StaffID.Size = new System.Drawing.Size(100, 20);
+            this.TB_StaffID.TabIndex = 2;
+            // 
+            // ST_StaffID
+            // 
+            this.ST_StaffID.AutoSize = true;
+            this.ST_StaffID.Location = new System.Drawing.Point(36, 48);
+            this.ST_StaffID.Name = "ST_StaffID";
+            this.ST_StaffID.Size = new System.Drawing.Size(43, 13);
+            this.ST_StaffID.TabIndex = 3;
+            this.ST_StaffID.Text = "Staff ID";
+            // 
+            // StaffDGV
+            // 
+            this.StaffDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StaffDGV.Location = new System.Drawing.Point(207, 22);
+            this.StaffDGV.Name = "StaffDGV";
+            this.StaffDGV.Size = new System.Drawing.Size(240, 150);
+            this.StaffDGV.TabIndex = 4;
+            this.StaffDGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.StaffDGV_CellContentClick);
+            // 
+            // StaffTasks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 195);
+            this.Controls.Add(this.StaffDGV);
+            this.Controls.Add(this.ST_StaffID);
+            this.Controls.Add(this.TB_StaffID);
+            this.Controls.Add(this.ST_Back);
+            this.Controls.Add(this.ST_Search);
+            this.Name = "StaffTasks";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.StaffDGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ST_Search;
+        private System.Windows.Forms.Button ST_Back;
+        private System.Windows.Forms.TextBox TB_StaffID;
+        private System.Windows.Forms.Label ST_StaffID;
+        private System.Windows.Forms.DataGridView StaffDGV;
+    }
+}
---
+++ SoftwareEngineering_Project/StaffTasks.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoftwareEngineering_Project
+{
+    public partial class StaffTasks : Form
+    {
+        static bool dateValChanged = false;
+        static int StaffTest = -1;
+
+        public StaffTasks()
+        {
+            InitializeComponent();
+        }
+
+        private void StaffDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void StaffDGV_SelectionChanged(object sender, EventArgs e)
+        {
+            DataGridView dgv = sender as DataGridView;
+            if (dgv.SelectedRows.Count == 1)
+            {
+                DataGridViewRow dRow = dgv.SelectedRows[0];
+                if (dRow.Cells["staffID"].Value.ToString().Length > 0)
+                {
+                    StaffTest = Convert.ToInt32(dRow.Cells["staffid"].Value.ToString());
+                }
+            }
+        }
+
+        private void StaffTasks_Load(object sender, EventArgs e)
+        {
+            dataGridSet("SELECT * from STAFF");
+            StaffDGV.SelectionChanged += new EventHandler(StaffDGV_SelectionChanged);
+        }
+
+        private void ST_Search_Click(object sender, EventArgs e)
+        {
+            string sqlString;
+            string StaffID;
+            bool display = false;
+
+            StaffID = TB_StaffID.Text;
+
+            sqlString = "SELECT * from STAFF WHERE ";
+
+            if (TB_StaffID.TextLength > 0)
+            {
+                sqlString += "STAFFID = '" + StaffID + "'";
+                display = true;
+            }
+
+            if (display) dataGridSet(sqlString);
+
+        }
+        private void dataGridSet(string sqlString)
+        {
+            DataSet dsPerson = DBConnection.getDBConnectionInstance().getDataSet(sqlString);
+
+            //get the table to be displayed from the data set
+            DataTable dtPerson = dsPerson.Tables[0];
+
+            //set the data source for the data grid view
+            StaffDGV.DataSource = dtPerson;
+        }
+
+        private void ST_Back_Click(object sender, EventArgs e)
+        {
+            Form[] prevPage;
+            prevPage = this.OwnedForms;
+            this.RemoveOwnedForm(prevPage[0]);
+            this.Close();
+            prevPage[0].Show();
+        }
+    }
+}
+    
